197f0c96860df23dfcdaee3175f2590c23d5114b
Mahdy Abrar Sharzy <80053701+masharzy@users.noreply.github.com>
2026-09-12 22:42:36 +0600

    Add site verification to metadata
    Add site verification key to metadata

--- apps/dashboard/app/layout.js
+++ apps/dashboard/app/layout.js
@@ -3,6 +3,7 @@ import { AuthProvider } from "@/components/auth-provider";
 export const metadata = {
   title: "The Unpirator",
   description: "Protected media infrastructure for modern platforms",
+  "unpirator-site-verification":"4gk2gIPKoyu29HiqO9JM26xv-Jr70SNk",
 };
 export default function RootLayout({ children }) {
   return (
